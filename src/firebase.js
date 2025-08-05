b6cc41f057118a2df322589e8aee886021f5dfc5
HariHaraPriyan07 <harapriyanhari@gmail.com>
2025-08-05 22:34:49 +0530

    firebase_update

--- src/firebase.js
+++ src/firebase.js
@@ -19,3 +19,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth(app);
+export { app, analytics, auth };
